0653dc0c5546f434e6dc8d9658c669548c49a566
Andrey <Hludeev.andrey1980@gmail.com>
2021-12-18 10:36:30 +0200

    update 15.12.2021 update 15.12.2021 22-03 update 16.12.2021 00-26 update 18.12.2021 10-36

--- test.py
+++ test.py
@@ -1,50 +1,46 @@
-from settings import hels, level, win_player, win_battle
 import random
 class Enemy:
-    def init(self, level):
+    def __init__(self, level):
         self.level = level
         self.lives = level
 
-    def SelectAttack(self):
-        return random.randint(1, 3)
+    def selectAttack(self):
+        enemy_attak = random.randint(1, 3)
+        return enemy_attak
+
     def DecreaseLives(self):
         self.lives = self.lives - 1
         if self.lives <= 0:
             raise EnemyDown()
 
-enemy1 = Enemy()
-enemy1.SelectAttack()
+    def attack(self, enemy_obj):
+        pl_attack = int(input("Сделайте выбор — (warrior = [1], mage[2], rogue[3]): "))
+        enem_attack = enemy_obj.selectAttack()
+        result = Enemy.fight(pl_attack,enem_attack)
+        return result
 
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.score = 0
-        self.life = help
-
-    def attack(self):
-        choice_player = int(input("Сделайте выбор — (warrior = [1], mage[2], rogue[3]): "))
-        return choice_player
-
-
-    def fight_attack(self,choice_player,enemy_obj):
-        if choice_player == enemy_obj:
+    @staticmethod
+    def fight(attack,defense):
+        if attack == defense:
             win = 0
-        if choice_player == 1 and enemy_obj == 2:
-            win = 0
-        if choice_player == 1 and enemy_obj == 3:
+        if attack == 1 and defense == 2:
+                win = -1
+        if attack == 1 and defense == 3:
             win = 1
-        if choice_player == 2 and enemy_obj == 1:
+        if attack == 2 and defense == 1:
             win = 1
-        if choice_player == 2 and enemy_obj == 3:
-            win = 0
-        if choice_player == 3 and enemy_obj == 1:
-            win = 0
-        if choice_player == 3 and enemy_obj == 2:
+        if attack == 2 and defense == 3:
+            win = -1
+        if attack == 3 and defense == 1:
+            win = -1
+        if attack == 3 and defense == 2:
             win = 1
-        if win == 0:
-            return 0
-        if win == 1:
-            return -1
+        return win
 
-batlle = Player("Wakim")
-print(batlle.fight_attack())
+
+
+
+
+enem1= Enemy(10)
+enem2 = Enemy(5)
+print(enem2.fight_attack())
